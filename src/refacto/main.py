10e3dbcc3f886c1474a4ca590afa55a3f3b7c363
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-09-02 23:24:16 +0200

    :construction: MVP

--- src/refacto/main.py
+++ src/refacto/main.py
@@ -1,11 +1,16 @@
 import typer
 
+from refacto.server import server
+
 app = typer.Typer()
+
+Host: str = typer.Option(default="localhost")
+Port: int = typer.Option(default=8080)
 
 
 @app.command()
-def welcome(name: str) -> None:
-    typer.echo(f"Welcome {name}!")
+def tcp(host: str = Host, port: int = Port) -> None:
+    server.start_tcp(host=host, port=port)
 
 
 if __name__ == "__main__":
