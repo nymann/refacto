7991231e05ce8f20bac20c1b08f4377f65718e2d
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-09-05 03:39:55 +0200

    :sparkles: Use the expression value instead of BinaryNode

--- src/refacto/refactorings/extract_variable.py
+++ src/refacto/refactorings/extract_variable.py
@@ -7,8 +7,7 @@ from refacto.refactorings.refactoring_utilities import get_chars_in_range
 
 
 class ExtractVariableTransformer(libcst.CSTTransformer):
-    def __init__(self, node: libcst.BinaryOperation, expr: libcst.Expr, variable_name: str) -> None:
-        self.node: libcst.BinaryOperation = node
+    def __init__(self, expr: libcst.Expr, variable_name: str) -> None:
         self.expr: libcst.Expr = expr
         self.variable_name = libcst.Name(variable_name)
         self.extract_next_statement_line = False
@@ -19,12 +18,21 @@ class ExtractVariableTransformer(libcst.CSTTransformer):
             return libcst.RemovalSentinel.REMOVE
         return updated_node
 
+    def leave_Call(self, original_node: libcst.Call, updated_node: libcst.Call) -> libcst.Call | libcst.Name:
+        if self.expr.value.deep_equals(original_node):
+            self.extract_next_statement_line = True
+            return self.variable_name
+        return updated_node
+
+    def visit_Call(self, node: libcst.Call) -> bool:
+        return not self.expr.value.deep_equals(node)
+
     def leave_BinaryOperation(
         self,
         original_node: libcst.BinaryOperation,
         updated_node: libcst.BinaryOperation,
     ) -> libcst.BinaryOperation | libcst.Name:
-        if self.node.deep_equals(original_node):
+        if self.expr.value.deep_equals(original_node):
             self.extract_next_statement_line = True
             return self.variable_name
         return updated_node
@@ -37,7 +45,7 @@ class ExtractVariableTransformer(libcst.CSTTransformer):
         if not self.extract_next_statement_line or self.extraced_once:
             return updated_node
         target = libcst.AssignTarget(self.variable_name)
-        assigned_variable = libcst.Assign(targets=[target], value=self.node)
+        assigned_variable = libcst.Assign(targets=[target], value=self.expr.value)
         extracted_statement = libcst.SimpleStatementLine(body=[assigned_variable])
         self.extract_next_statement_line = False
         self.extraced_once = True
@@ -47,12 +55,7 @@ class ExtractVariableTransformer(libcst.CSTTransformer):
 class ExpressionFinder(libcst.CSTVisitor):
     def __init__(self) -> None:
         super().__init__()
-        self.node: libcst.BinaryOperation | None = None
         self.expr: libcst.Expr | None = None
-
-    def visit_BinaryOperation(self, node: libcst.BinaryOperation) -> Optional[bool]:
-        self.node = node
-        return False
 
     def visit_Expr(self, node: libcst.Expr) -> Optional[bool]:
         self.expr = node
@@ -63,10 +66,10 @@ def extract_variable(selected_range: Range, source: str) -> str:
     range_tree = libcst.parse_module(get_chars_in_range(selected_range=selected_range, source=source))
     visitor = ExpressionFinder()
     range_tree.visit(visitor=visitor)
-    if visitor.node is None or visitor.expr is None:
+    if visitor.expr is None:
         raise RuntimeError("Node was unexpectically None!")
 
     source_tree = libcst.parse_module(source=source)
-    extract_transformer = ExtractVariableTransformer(node=visitor.node, expr=visitor.expr, variable_name="a")
+    extract_transformer = ExtractVariableTransformer(expr=visitor.expr, variable_name="a")
     transformed_tree = source_tree.visit(extract_transformer)
     return transformed_tree.code
---
+++ tests/test_cases/extract_variable/book_example_shipping_price/before.py
@@ -0,0 +1,4 @@
+def price(order) -> float:
+    base_price = order.quantity * order.item_price
+    quantity_discount = max(0, order.quantity - 500) * order.item_price * 0.05
+    return base_price - quantity_discount + min(base_price * 0.1, 100)
